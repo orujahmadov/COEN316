4c826d3d318c47eea97ae46a8d928b2bc807dfac
Oruj Ahmadov <o_ahmado@gecko.encs.concordia.ca>
2017-11-06 10:58:30 -0500

    lab 3 done yay

--- Lab3/next_address.vhdl
+++ Lab3/next_address.vhdl
@@ -1,7 +1,7 @@
--- Next address calculator for PC
+-- Next address calculator for 
 library IEEE;
 use IEEE.std_logic_1164.all;
-use IEEE.std_logic_unsigned.all;
+use IEEE.std_logic_signed.all;
 use ieee.numeric_std.all;
 
 entity next_address is
@@ -20,13 +20,9 @@ architecture NAImplementation of next_address is
 begin
 
 	next_address_pr: process (rt, rs, pc, target_address, branch_type, pc_sel)
-
-		variable pc: unsigned(31 downto 0);
 		variable offset_value: unsigned(31 downto 0);
 
 	begin
-
-		pc := unsigned(pc);
 
 		if (target_address(15) = '0') then
 			offset_value := unsigned("0000000000000000" & target_address(15 downto 0));
@@ -36,57 +32,57 @@ begin
 
 		case pc_sel is
 
-      -- no jump (PC +1)
+      			-- no jump (PC +1)
 			when "00" =>
 
 				case branch_type is
 
 					when "00" =>
-						next_pc <= std_logic_vector(pc + 1); --no branching, pc += 1
+						next_pc <= std_logic_vector(unsigned(pc) + 1); --no branching, pc += 1
 
-          -- BEQ
+          				-- BEQ
 					when "01" =>
-            -- IF rs=rt, 1 + branch offset value sign extended to 32 bits
+            				-- IF rs=rt, 1 + branch offset value sign extended to 32 bits
 						if (rs = rt) then
-							next_pc <= std_logic_vector(pc + 1 + offset_value);
+							next_pc <= std_logic_vector(unsigned(pc) + 1 + offset_value);
 						else
-							next_pc <= std_logic_vector(pc + 1);
+							next_pc <= std_logic_vector(unsigned(pc) + 1);
 						end if;
 
-          --BNE
+          				--BNE
 					when "10" =>
-          -- IF rs/=rt, 1 + branch offset value sign extended to 32 bits
+          				-- IF rs/=rt, 1 + branch offset value sign extended to 32 bits
 						if (rs /= rt) then
-							next_pc <= std_logic_vector(pc + 1 + offset_value);
+							next_pc <= std_logic_vector(unsigned(pc) + 1 + offset_value);
 						else
-							next_pc <= std_logic_vector(pc + 1);
+							next_pc <= std_logic_vector(unsigned(pc) + 1);
 						end if;
 
-          -- BLTZ
+          				-- BLTZ
 					when "11" =>
-          -- IF rs<0, 1 + branch offset value sign extended to 32 bits
+          				-- IF rs<0, 1 + branch offset value sign extended to 32 bits
 						if (signed(rs) < 0) then
-							next_pc <= std_logic_vector(pc + 1 + offset_value);
+							next_pc <= std_logic_vector(unsigned(pc) + 1 + offset_value);
 						else
-							next_pc <= std_logic_vector(pc + 1);
+							next_pc <= std_logic_vector(unsigned(pc) + 1);
 						end if;
 
 					when others =>
-						next_pc <= std_logic_vector(pc + 1);
+						next_pc <= std_logic_vector(unsigned(pc) + 1);
 
 				end case;
 
-      -- Jump
+      			-- Jump
 			when "01" =>
 				next_pc <= "000000" & target_address; -- Padding target with zeros
 
-      -- Jump Register
+      			-- Jump Register
 			when "10" => -- jump register: PC = contents of register rs
 				next_pc <= rs;
 
-      -- Not used
+      			-- Not used
 			when others => -- shouldn't happen, PC = PC + 1
-				next_pc <= std_logic_vector(pc + 1);
+				next_pc <= std_logic_vector(unsigned(pc) + 1);
 
 		end case;
 
